944d5f92800c071091273ec794651f59b8e9e96f
sukshiti23 <sukshiti2306@gmail.com>
2025-07-23 20:42:51 +0530

    Added code files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tictactoe from './components/TicTacToe/tictactoe'
+
+
+const App = () => {
+  return (
+    <div>
+      <Tictactoe/>
+    </div>
+  )
+}
+
+export default App
